93c2c7d81f86b4887ef6b065b1ded8c442b99db3
Jesse Erwin <jjerwin12@gmail.com>
2025-08-23 21:27:35 -0400

    Initial Commit

---
+++ BudgetFriendly/BudgetFriendlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetFriendlyApp.swift
+//  BudgetFriendly
+//
+//  Created by Jesse Erwin on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetFriendlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetFriendlyTests/BudgetFriendlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetFriendlyTests.swift
+//  BudgetFriendlyTests
+//
+//  Created by Jesse Erwin on 8/23/25.
+//
+
+import Testing
+@testable import BudgetFriendly
+
+struct BudgetFriendlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
