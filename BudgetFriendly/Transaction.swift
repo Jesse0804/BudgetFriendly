8d2aa83d451ae39971e255bdf007f766155aacae
Jesse Erwin <jjerwin12@gmail.com>
2025-08-26 23:06:08 -0400

    Created category cases for transaction forms

--- BudgetFriendly/Transaction.swift
+++ BudgetFriendly/Transaction.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
+enum Category: String, CaseIterable, Identifiable {
+    case groceries = "Groceries"
+    case food = "Food"
+    case gas = "Gas"
+    case entertainment = "Entertainment"
+    case shopping = "Shopping"
+    case utilities = "Utilities"
+    case bills = "Bills"
+    case savings = "Savings"
+    case other = "Other"
+    
+    var id: String { self.rawValue }
+}
+
 struct Transaction: Identifiable {
     let id = UUID()
     let description: String
     let amount: Double
     let date: Date
+    let category: Category
 }
